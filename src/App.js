824d6581bbe70743f90ce9b593284d0faae6014e
elektron1k <fop.holoborshchyi@gmail.com>
2023-03-31 23:04:34 +0300

    second

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
     [true, 'first task', 'second task'],
     [false, 'first task', 'second task'],
-    'Text with link. &&&It is link&&https://www.google.com/&&& Text after link ',
+    'Text with link. &&&It is link.&&https://www.google.com/&&& Text after link ',
   ];
 
   const forString = (el) => {
@@ -29,6 +29,7 @@ function App() {
       <p>
         {el.slice(0, indexStartTextOfLink)}
         <a href={link}>{textOfLink}</a>
+        {el.slice(indexEndOfLink + 3, el.length)}
       </p>
     );
   };
